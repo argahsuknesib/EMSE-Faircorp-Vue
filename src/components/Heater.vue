9b1fc021ef940d4e5ba34631b376c4e485f7e29c
Kushagra Singh BISEN <k.bisen@yahoo.com>
2021-12-30 11:32:51 +0100

    non working get method

---
+++ src/components/Heater.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="heater">
+    <table class="table">
+      <thead>
+        <tr>
+          <th v-for="key in columns" v-bind:key="key">
+            {{ key }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr v-for="values in this.heaters" v-bind:key="values.id">
+              <td>{{ values.id }}</td>
+              <td>{{ values.name }}</td>
+              <td>{{ values.power}}</td>
+          </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { API_KUSHAGRA } from "../config";
+
+export default {
+  name: "heater",
+  props: ["room"],
+  data() {
+    return {
+      heaters: [],
+      columns: [
+        "ID",
+        "Name",
+        "Power",
+        "Heater Status",
+        "Room Name",
+        "Room ID",
+        "Room",
+      ],
+      key: "",
+    };
+  },
+
+  async function() {
+    let response = await axios.get(`${API_KUSHAGRA}/api/heater`);
+    this.heaters = response.data;
+    heaters = this.heaters;
+  },
+};
+</script>
+
+<style>
+@import "../assets/css/table.css";
+.clickable {
+  cursor: pointer;
+}
+td {
+  padding: 10px;
+}
+</style>
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const API_KUSHAGRA = 'http://app-29838324-aff3-48c1-97cd-42b28e639455.cleverapps.io/'
