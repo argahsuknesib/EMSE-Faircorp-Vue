a03d3ce5948b83a0ec8bed67aa187f05abae341b
Kushagra Singh BISEN <k.bisen@yahoo.com>
2022-01-03 02:41:09 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,14 @@
 <template>
   <div id="app">
-<!-- <h1>Hello, this is Kushagra</h1> -->
-<!-- <Header/> -->
-
 <router-view></router-view>
-
-    <!-- <Location/> -->
-    <!-- <Navigation/> -->
-    <!-- <Building /> -->
-    <!-- <Hello /> -->
-    <!-- <Heater/> -->
-    <!-- <Developer/> -->
-    <!-- <Weather/> -->
-    <!-- <Location/> -->
   </div>
 </template>
 
 <script>
-// import Heater from './components/Heater.vue'
-// import Navigation from './components/Navigation.vue'
-// import Location from './components/Location.vue'
-// import Building from './components/Building.vue'
-// import Hello from './components/Hello.vue'
-// import Developer from './components/Developer.vue'
-// import Location from './components/Location.vue'
-// import Weather from './components/Weather.vue'
-// import Header from './components/Header.vue'
 
 export default {
   name: "App",
   components: {
-    // Header
-    // Weather
-    // Location
-    // Developer
-    // Hello
-    // Building
-    // Navigation,
-    // Location,
-    // Heater
   },
 
   data() {},
